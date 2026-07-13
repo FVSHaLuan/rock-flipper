9c1e8e42a11393219db0f843bee8900614afcd0e
FVS Hà Luận <fvs922@gmail.com>
2026-07-13 11:32:21 +0700

    - Updated

--- Rock Flipper/Assets/_AMainGame/Scripts/Run/Player Cursor/PlayerCursor.cs
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Player Cursor/PlayerCursor.cs
@@ -4,7 +4,11 @@ namespace Agame.Run.Combat
 {
     public class PlayerCursor : ExtendedMonoBehaviourRun
     {
-     
+        protected void Update()
+        {
+            var th = transformHandle;
+            th.position = entry.GetPointerPositionViaConversionCamera();
+        }
     }
 
 }
