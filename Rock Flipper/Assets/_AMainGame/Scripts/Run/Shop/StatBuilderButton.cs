3b72f8dec59d4b5941ada6ef8b78ea7711f6a9cd
FVS Hà Luận <fvs922@gmail.com>
2026-07-29 10:44:02 +0700

    - Updated

---
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Shop/StatBuilderButton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Agame.Run
+{
+    public class StatBuilderButton : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
