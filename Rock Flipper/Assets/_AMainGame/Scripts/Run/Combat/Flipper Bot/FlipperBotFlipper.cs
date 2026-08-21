267f01a65528fb7885fffc4a6338dece4d2b3deb
FVS Hà Luận <fvs922@gmail.com>
2026-08-21 11:31:56 +0700

    - Updated

---
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Combat/Flipper Bot/FlipperBotFlipper.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Agame.Run.Combat
+{
+    public class FlipperBotFlipper : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
