bb0f76d94eec28334fac08fb773286d668408f94
FVS Hà Luận <fvs922@gmail.com>
2026-08-21 12:12:44 +0700

    - Updated

--- Rock Flipper/Assets/_AMainGame/Scripts/Run/Combat/Flipper Bot/FlipperBot.cs
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Combat/Flipper Bot/FlipperBot.cs
@@ -7,6 +7,30 @@ namespace Agame.Run.Combat
         [Header("Components")]
         [SerializeField]
         private FlipperBotFlipper flipper;
+
+        private CooldownObject flipCooldown;
+
+        protected void Start()
+        {
+            flipCooldown = new CooldownObject(BuildStats.flipperBotFlippingInterval);
+            flipCooldown.StartCoolingDown();
+        }
+
+        protected void Update()
+        {
+            UpdateFlipper();
+        }
+
+        private void UpdateFlipper()
+        {
+            flipCooldown.CooldownTime = BuildStats.flipperBotFlippingInterval;
+            flipCooldown.Update(Time.deltaTime);
+            if (!flipCooldown.IsCoolingDown)
+            {
+                flipper.TryToFlipRocks();
+                flipCooldown.StartCoolingDown();
+            }
+        }
     }
 
 }
