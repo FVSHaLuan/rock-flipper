7b3963c318500ffd377bce0e4878e6446760415d
FVS Hà Luận <fvs922@gmail.com>
2026-08-21 21:46:14 +0700

    - Updated

--- Rock Flipper/Assets/_AMainGame/Scripts/Run/Combat/Flipper Bot/FlipperBotMovement.cs
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Combat/Flipper Bot/FlipperBotMovement.cs
@@ -6,6 +6,33 @@ namespace Agame.Run.Combat
     {
         private float Speed => BuildStats.flipperBotMovementSpeed;
 
+        private Vector2 currentTarget;
+        private bool hasTarget;
+
+        protected void Update()
+        {
+            ChaseTarget();
+        }
+
+        private void ChaseTarget()
+        {
+            ///
+            if (!hasTarget)
+            {
+                currentTarget = GetNextTarget();
+                hasTarget = true;
+            }
+
+            ///
+            transform.position = Vector2.MoveTowards(transform.position, currentTarget, Speed * Time.deltaTime);
+
+            ///
+            if ((Vector2)transform.position == currentTarget)
+            {
+                currentTarget = GetNextTarget();
+            }
+        }
+
         private Vector2 GetNextTarget()
         {
             return Playfield.GetRandomPoint(-Vector2.one * 0.2f);
