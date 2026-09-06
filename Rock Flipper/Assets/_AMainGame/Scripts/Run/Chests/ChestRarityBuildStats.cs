98672cd6130dfa00c749f3b5b6f502edcc029bd9
FVS Hà Luận <fvs922@gmail.com>
2026-09-06 11:23:45 +0700

    - Updated

---
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Chests/ChestRarityBuildStats.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Agame.Run.Combat
+{
+    [System.Serializable]
+    public class ChestRarityBuildStats
+    {
+        public float weight;
+        public int maxHp;
+    }
+
+}
