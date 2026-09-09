5d043597053b4edf7fd4686e52f95d92211e8984
FVS Hà Luận <fvs922@gmail.com>
2026-09-09 10:05:33 +0700

    - Updated

--- Rock Flipper/Assets/_AMainGame/Scripts/Run/Shop/Items/ItemView.cs
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Shop/Items/ItemView.cs
@@ -1,14 +1,29 @@
+using GD;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Agame.Run.Shop
 {
     public class ItemView : ExtendedMonoBehaviourRun
     {
+        [SerializeField]
+        private Image itemIconImage;
+        [SerializeField]
+        private GameObject progressBarWrapper;
+        [SerializeField]
+        private ProgressBar progressBar;
+        [SerializeField]
+        private GameObject maxedText;
+
         private ItemData itemData;
 
         public void SetItemData(ItemData itemData)
         {
+            ///
             this.itemData = itemData;
+
+            ///
+            itemIconImage.sprite = itemData.ItemIcon;
         }
     }
 
