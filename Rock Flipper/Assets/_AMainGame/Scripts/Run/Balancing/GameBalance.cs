b2c6c4fc9dd3a0f1df68f018aef5a4919520e4bd
FVS Hà Luận <fvs922@gmail.com>
2026-09-09 11:26:46 +0700

    - Updated

--- Rock Flipper/Assets/_AMainGame/Scripts/Run/Balancing/GameBalance.cs
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Balancing/GameBalance.cs
@@ -1,9 +1,27 @@
+using Agame.Run.Combat;
 using UnityEngine;
 
 namespace Agame.Balancing
 {
     public partial class GameBalance : ScriptableObjectWithInit
     {
+        public int ItemRequiredExpForNextLevel(int currentLevel)
+        {
+            if (currentLevel < 0)
+            {
+                Debug.LogError("Current level cannot be negative.");
+                return 0;
+            }
+
+            ///
+            return 10;
+        }
+
+        public int GetItemMaxLevel(ChestRarity chestRarity)
+        {
+            return 20;
+        }
+
         public double GetRequiredExpForNextLevel(int currentLevel)
         {
             if (currentLevel < 0)
