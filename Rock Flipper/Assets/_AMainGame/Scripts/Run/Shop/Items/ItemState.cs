bbccc2c8d0801d1ff71a89257686485a2894b51b
FVS Hà Luận <fvs922@gmail.com>
2026-09-09 11:00:09 +0700

    - Updated

---
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Shop/Items/ItemState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Agame.Run.Shop
+{
+    [System.Serializable]
+    public struct ItemState
+    {
+        public string itemId;
+        public int level;
+        public int exp;
+    }
+
+}
