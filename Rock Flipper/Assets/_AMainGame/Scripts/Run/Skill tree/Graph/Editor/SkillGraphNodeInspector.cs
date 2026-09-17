29d1f2c02d1f2e24f8b38bc42c57f8527adc46be
FVS Hà Luận <fvs922@gmail.com>
2026-09-17 10:04:43 +0700

    - Updated

--- Rock Flipper/Assets/_AMainGame/Scripts/Run/Skill tree/Graph/Editor/SkillGraphNodeInspector.cs
+++ Rock Flipper/Assets/_AMainGame/Scripts/Run/Skill tree/Graph/Editor/SkillGraphNodeInspector.cs
@@ -13,9 +13,27 @@ namespace Agame.Run
             base.DrawTopButtons();
 
             ///
+            var skillGraphNode = target as SkillGraphNode;
+
+            ///
+            if (skillGraphNode == null)
+            {
+                return;
+            }
+
+            ///
             if (GUILayout.Button("Fill costs", GUILayout.Height(40)))
             {
-                (target as SkillGraphNode).Editor_FillCosts();
+                skillGraphNode.Editor_FillCosts();
+            }
+
+            ///
+            using (new EditorGUI.DisabledGroupScope(skillGraphNode.BuildAgent == null))
+            {
+                if (GUILayout.Button("View Build Agent", GUILayout.Height(40)))
+                {
+                    EditorUtility.OpenPropertyEditor(skillGraphNode.BuildAgent.gameObject);
+                }
             }
         }
     }
